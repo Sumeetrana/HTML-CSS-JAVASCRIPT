360b42f5490f01809944db3cd1d450d0bf3fe671
Sumeetrana <sumeetsinh28@gmail.com>
2020-12-22 22:16:00 +0530

    Timer(Stopwatch) completed

--- timer/index.js
+++ timer/index.js
@@ -1,13 +1,20 @@
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
+const circle = document.querySelector("circle");
 
+const perimeter = circle.getAttribute("r") * 2 * Math.PI;
+circle.setAttribute("stroke-dasharray", perimeter);
+
+let currentOffset = 0;
+let duration;
 const timer = new Timer(durationInput, startButton, pauseButton, {
-  onStart() {
-    console.log("TImer has started!");
+  onStart(totalDuration) {
+    duration = totalDuration;
   },
-  onTick() {
-    console.log("Timer has ticked down");
+  onTick(timeRemaining) {
+    currentOffset = (perimeter * timeRemaining) / duration - perimeter;
+    circle.setAttribute("stroke-dashoffset", currentOffset);
   },
   onComplete() {
     console.log("Timer has completed!");
